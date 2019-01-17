c1ab3ccefa400fc1e5ef1f9867dfa1cdb7e5cbe0
czajmen <kamil.czaja94@gmail.com>
2019-01-17 18:02:13 +0100

    Deployment addings

--- api/api/__init__.py
+++
@@ -1,7 +0,0 @@
-# from flask_restful import Api
-# from app import  app
-# from .ThingsController import  ThingsController
-#
-# restServer = Api(app)
-# restServer.add_resource(ThingsController, "/api/things")
-# restServer.add_resource(ThingsController, "/api/things/id/<string:id>")
--- api/app.py
+++ api/app.py
@@ -12,8 +12,4 @@ from api import ThingsController
 
 
 if __name__ == '__main__':
-    app.debug = True
-    app.config['DATABASE_NAME'] = 'library.db'
-    host = os.environ.get('IP', '0.0.0.0')
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host=host, port=port)
+    app.run()
