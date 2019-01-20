d5e699ea95a4fa6b200355772fa8a7ed607e7726
Wojciech Kryg <krygwojciech@gmail.com>
2019-01-20 17:18:45 +0100

    Add Google Sign In library and configuration with Firebase

--- mobile/lost_things/ios/Runner/GeneratedPluginRegistrant.m
+++ mobile/lost_things/ios/Runner/GeneratedPluginRegistrant.m
@@ -3,10 +3,12 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
+#import <google_sign_in/GoogleSignInPlugin.h>
 
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FLTGoogleSignInPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTGoogleSignInPlugin"]];
 }
 
 @end
