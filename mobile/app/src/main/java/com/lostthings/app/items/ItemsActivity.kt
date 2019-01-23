1a65f12d6e1db90f4775b08e436f984033acbc2c
Wojciech Kryg <krygwojciech@gmail.com>
2019-01-23 20:07:30 +0100

    Add loading items

--- mobile/app/src/main/java/com/lostthings/app/items/ItemsActivity.kt
+++ mobile/app/src/main/java/com/lostthings/app/items/ItemsActivity.kt
@@ -1,15 +1,36 @@
 package com.lostthings.app.items
 
 import android.os.Bundle
+import android.util.DisplayMetrics
+import androidx.recyclerview.widget.GridLayoutManager
 import com.lostthings.R
 import com.lostthings.app.base.BaseActivity
 import kotlinx.android.synthetic.main.activity_items.*
 
 class ItemsActivity : BaseActivity() {
 
+    private lateinit var adapter: ItemAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_items)
-        repository.getItems({ items.text = it.component1().name }, { it.toString() })
+        setupItemsRv()
+        repository.getItems({ adapter.setData(it) }, { it.toString() })
+    }
+
+    private fun setupItemsRv() {
+        adapter = ItemAdapter {}
+        itemsItemsRv.layoutManager = GridLayoutManager(this, calculateNumberOfColumns())
+        itemsItemsRv.adapter = adapter
+    }
+
+    private fun calculateNumberOfColumns(): Int {
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        return displayMetrics.widthPixels / COLUMN_WIDTH_DIVIDER
+    }
+
+    companion object {
+        private const val COLUMN_WIDTH_DIVIDER = 500
     }
 }
