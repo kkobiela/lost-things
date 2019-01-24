95bc9b4ce84cf03b3d12d8fce8c3b57aa4a934c9
Wojciech Kryg <krygwojciech@gmail.com>
2019-01-24 20:54:03 +0100

    Add capture photo

--- mobile/app/src/main/java/com/lostthings/app/add/AddItemActivity.kt
+++ mobile/app/src/main/java/com/lostthings/app/add/AddItemActivity.kt
@@ -1,13 +1,22 @@
 package com.lostthings.app.add
 
 
+import android.Manifest
+import android.app.Activity
 import android.app.DatePickerDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 import com.lostthings.R
 import com.lostthings.app.base.BaseActivity
 import kotlinx.android.synthetic.main.activity_add_item.*
+import kotlinx.android.synthetic.main.activity_profile.*
 import java.text.SimpleDateFormat
 import java.util.*
+
 
 class AddItemActivity : BaseActivity() {
 
@@ -18,6 +27,49 @@ class AddItemActivity : BaseActivity() {
         setContentView(R.layout.activity_add_item)
         title = "Dodaj znalezioną rzecz"
         setupDatePicker()
+        setupAddItem()
+        setupAddPhoto()
+    }
+
+    private fun setupAddPhoto() {
+        addPhotoFab.setOnClickListener {
+            if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(
+                    arrayOf(Manifest.permission.CAMERA),
+                    CAMERA_PERMISSION_CODE
+                )
+            } else {
+                val cameraIntent = Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                val cameraIntent = Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE)
+            } else {
+                Toast.makeText(this, "Nie dodasz zdjęcia :c", Toast.LENGTH_LONG).show()
+            }
+
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CAMERA_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            val photo = data?.extras?.get("data") as? Bitmap
+            val drawable = RoundedBitmapDrawableFactory.create(resources, photo)
+            drawable.isCircular = true
+            photoIv.setImageDrawable(drawable)
+        }
+    }
+
+    private fun setupAddItem() {
+        // TODO: Add item
     }
 
     private fun setupDatePicker() {
@@ -40,5 +92,10 @@ class AddItemActivity : BaseActivity() {
         val format = "dd.MM.yyyy"
         val formatter = SimpleDateFormat(format, Locale.getDefault())
         foundDateTv.setText(formatter.format(calendar.time))
+    }
+
+    companion object {
+        private const val CAMERA_PERMISSION_CODE = 123
+        private const val CAMERA_REQUEST_CODE = 321
     }
 }
